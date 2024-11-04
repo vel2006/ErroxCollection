4cae607c97cafeff79b59a01440d56adf27548b2
That1EthicalHacker <116861198+vel2006@users.noreply.github.com>
2024-11-04 13:15:07 -0600

    Update ErroxShell.py
    Updating ErroxShell.py to also include XOR encryption (doesnt work all that well...)

--- ErroxShell/ErroxShell.py
+++ ErroxShell/ErroxShell.py
@@ -1,20 +1,56 @@
+import threading
 import platform
+import random
+import time
 import sys
 import os
 
-info_head = "[i] INFO: "
-eror_head = "[i] EROR: "
-misc_head = "[*] MISC: "
-inpt_head = "[^] INPT: "
+inpt_head = "\033[33m[^] INPT: \033[0m"
+info_head = "\033[32m[i] INFO: \033[0m"
+eror_head = "\033[31m[!] EROR: \033[0m"
+misc_head = "\033[30m[*] MISC: \033[0m"
 
-def get_inbetween(input_text, input_starting_point, input_ending_point):
-    start_index = input_text.find(input_starting_point)
-    end_index = input_text.find(input_ending_point, start_index + len(input_text))
-    if start_index != -1 and end_index != 1:
-        return input_text[start_index + len(input_starting_point):end_index]
-    else:
-        print(f"{eror_head}index for get_inbetween returned -1, ending script.")
-        exit(1)
+def gen_xor_key():
+    print(f"{info_head}Creating key...")
+    print(f"{misc_head}Getting values...")
+    nano_system_time = time.time_ns()
+    nano_process_time = time.process_time_ns()
+    print(f"{misc_head}Doing a random length sleep for more randomness...")
+    time.sleep(random.randint(0, random.randint(1, 10)))
+    nano_thread_time = time.thread_time_ns()
+    thread_id = threading.get_ident()
+    cpu_state = int(psutil.cpu_percent(interval=1))
+    allowed_selections = [nano_system_time, nano_process_time, nano_thread_time, thread_id, cpu_state]
+    picked_selections = []
+    print(f"{misc_head}Gotten values.")
+    print(f"{misc_head}Orginizing values...")
+    for i in range(len(allowed_selections)):
+        choice = random.choice(allowed_selections)
+        picked_selections.append(choice)
+        allowed_selections.remove(choice)
+    print(f"{misc_head}Done orginizing values.")
+    print(f"{misc_head}Getting long key...")
+    temp = ((picked_selections[0] ^ picked_selections[1]) ^ picked_selections[2]) ^ picked_selections[3] * picked_selections[4]
+    temp = temp.to_bytes((temp.bit_length() + 7) // 8, 'big')
+    print(f"{misc_head}Dont getting long key.")
+    print(f"{misc_head}Converting long key into bytes...")
+    key_array = []
+    for byte in temp:
+        temp_value = "0x{:02X}(".format(byte)
+        key_array.append(temp_value[temp_value.find("x") + len("x"):temp_value.find("(", temp_value.find("x") + len("x") + len(temp_value))])
+    print(f"{misc_head}Converted long key into bytes.")
+    print(f"{misc_head}Picking random byte from bytes to be key...")
+    key = random.choice(key_array)
+    print(f"{info_head}Created key.")
+    return key
+
+def xor_encryption(input_data, key):
+    output_array = []
+    print(f"{info_head}Starting XOR encryption on data...")
+    for byte in input_data:
+        output_array.append(format(int(byte, 16) ^ int(key, 16), "02X"))
+    print(f"{info_head}Done doing XOR encryption on data.")
+    return output_array
 
 def extract_shell_code(target_file):
     print(f"{info_head}Starting extraction of shell code...")
@@ -55,7 +91,7 @@ def extract_shell_code(target_file):
     with open("shellcode.bin", 'rb') as file:
         byte = file.read(1)
         while byte:
-            raw_shell_code.append(byte.hex())
+            raw_shell_code.append(byte.hex().upper())
             byte = file.read(1)
         file.close()
     print(f"{misc_head}Creating a hex form of \'shellcode.bin\'...")
@@ -64,20 +100,44 @@ def extract_shell_code(target_file):
             file.write(f"{byte}\n")
         file.close()
     print(f"{misc_head}Wrote hex for of \'shellcode.bin\' to \'hex_shellcode.txt\'.")
+    key = gen_xor_key()
+    encrypted_hex_shell_code = xor_encryption(raw_shell_code, key)
+    print(f"{misc_head}Writing XOR encrypted hex to \'encrypted_hex.txt\'...")
+    with open("encrypted_hex.txt", 'w') as file:
+        for byte in encrypted_hex_shell_code:
+            file.write(f"{byte}\n")
+        file.close()
+    print(f"{misc_head}Wrote XOR encrypted hex to \'encrypted_hex.txt\'.")
+    print(f"{misc_head}Checking for reverseability on XOR encrypted hex...")
+    reversed_encrypted_hex_shell_code = xor_encryption(encrypted_hex_shell_code, key)
+    if reversed_encrypted_hex_shell_code == encrypted_hex_shell_code:
+        print(f"{misc_head}Encrypted XOR hex is reversable.")
+    else:
+        print(f"{eror_head}Encrypted XOR hex is not reversable.")
+
 if __name__ == '__main__':
+    print(" _____                     ____  _          _ _ ")
+    print("| ____|_ __ _ __ _____  __/ ___|| |__   ___| | |")
+    print("|  _| | '__| '__/ _ \\ \\/ /\\___ \\| '_ \\ / _ \\ | |")
+    print("| |___| |  | | | (_) >  <  ___) | | | |  __/ | |")
+    print("|_____|_|  |_|  \\___/_/\\_\\|____/|_| |_|\\___|_|_|")
+    print("                         By: That1EthicalHacker")
+    print("")
     try:
         import pefile
+        import psutil
     except:
         with open("requirements.txt", "w") as file:
-            file.write("pefile==2023.2.7\n")
+            file.write("pefile==2023.2.7\npsutil==6.0.0\n")
             file.close()
-        print(f"{info_head}Installing needed package \'pefile\'...")
+        print(f"{info_head}Installing needed packages \'pefile\' and \'psutil\'...")
         if 'Windows' in platform.platform():
             os.system('python -m pip install -r requirements.txt')
         else:
             os.system('pip3 install -r requirements.txt')
         print(f"{info_head}Installed package \'pefile\'.")
     import pefile
+    import psutil
     if len(sys.argv) != 2:
         print(f"{info_head}usage: python(3) ErroxShell.py <exe_file_path>")
         exit(1)
